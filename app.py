bd8474d0a8a87f7946e49ae022b0f347c9c77024
Nasir Mustapha <teboy.com@gmail.com>
2023-04-19 18:07:39 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_socketio import SocketIO, send, join_room, leave_room
+
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+# setup a socketio server that only allows requests from the localhost and selected origins
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('join')
+def on_join(data):
+    room = data.get('user_id')
+    join_room(room)
+
+@socketio.on('leave')
+def on_leave(data):
+    room = data.get('user_id')
+    leave_room(room)
+
+@socketio.on('json')
+def handle_note(data):
+    send({'note':data.get('note')}, broadcast=True, json=True, room=data.get('note').get('creator_id'))
+
+@app.route('/')
+def index():
+    return {"name":"KlassNaut Socket Server", "version":"0.0.1", "status":"OK"}
+
+if __name__ == '__main__':
+    socketio.run(app)
